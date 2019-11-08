381e8ae2f33324b96385a271dccfa41c120a6594
SolaTyolo <solat.yolo@gmail.com>
2019-11-08 17:24:10 +0800

    feature: (  abstract loadCommand function )

--- src/Application.php
+++ src/Application.php
@@ -6,17 +6,38 @@ use Symfony\Component\Console\Application as SymfonyApplication ;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use LegoCue\Framework\Kernel;
+use LegoCue\Framework\Exception\LegoException;
 
 class Application extends SymfonyApplication
 {
+    /**
+     * @var LegoCue\Framework\Kernel
+     */
+    private $_commands = null;
+
+    /**
+     * entry function
+     */
     public function terminate( InputInterface $input = null, OutputInterface $output = null )
     {
-        $kernel = new Kernel();
+        if( is_null( $this->_commands ) || !($this->_commands instanceof Kernel ) )
+        {
+            throw new LegoException( 'commonds not loaded' );
+        }
+
         $commandLoader = new ContainerCommandLoader(
-            $kernel->getContainer(),
-            $kernel->getMaps()
+            $this->_commands->getContainer(),
+            $this->_commands->getMaps()
         );
         $this->setCommandLoader( $commandLoader );
         $this->run( $input, $output );
+    }
+
+    /**
+     * load commands-kernel class
+     */
+    public function loadCommands( Kernel $commands )
+    {
+        $this->_commands = $commands;
     }
 }
